39eb44cc886305ed8e32a3ce290b4b17dda81d92
EwigeFinsternis <ewigefinsternis@ngs.ru>
2012-03-30 01:35:20 +0600

    work basket

--- application/views/basket.php
+++ application/views/basket.php
@@ -8,14 +8,15 @@
             echo "<div id='empty'> Корзина пуста </div>";
         else{
             $total=0;
-            foreach($orderProducts as $product){
+            foreach($orderProducts as $key=>$product){
                 $data['product']=$product;
+                $data['key']=$key;
                 $this->load->view('basket_item', $data); 
                 $total += $product->price;
             }
            echo "<div id='makeOrder'>";
            echo "<a href='#'><img src='/images/basket_make_order.png' align='left'/></a>";
-           echo "<p id='total> P.</p></div>";
+           echo "<p id='totally'>".(string)$total." P.</p></div>";
         }
     }
     else //ни разу не добавляли товар в корзину
--- application/views/basket_item.php
+++ application/views/basket_item.php
@@ -1,10 +1,30 @@
 <div class="itemInfo">
     <form name="orderItem" method="post">
+        <input type="hidden" name="pr" value="<?=$key?>" />
         <img src="<?=$product->getImageUrl()?>" class="orderImg" />
-        <img onCLick="orderItem.submit()" src="/images/basket_remove.png" class="btnRmv"  align="right"/>
+        <!--<a href="javascript:void(0);"><img onCLick="orderItem.submit();" src="/images/basket_remove.png" class="btnRmv"  align="right"/></a>
+        -->
+        <input type="image" name="btnDel" src="/images/basket_remove.png" class="btnRmv"  align="right" />
         <h1> <?php echo $product->name; ?> </h1>
         <p><?php echo $product->description; ?></p>
         <div id="choosen">Выбранный размер:</div><div class="ch_size" ><?php echo $product->orderedSize; ?></div>
         <div id="price"> <?php echo ($product->price)." Р."; ?></div>
+        
+        <?php 
+            if (isset($_POST['btnDel_x'])){
+                 unset($_POST['btnDel_x']);
+                 $key = $_POST['pr'];
+               
+                 $orderInfo = $_SESSION['basketStuff']->getOrderInfo();
+                 $orderProducts = $orderInfo->orderedProductsInfo;
+                 $product = $orderProducts[$key]; 
+               //  print_r($product);
+                 
+               // die((string)$key);
+                //die((string)$_POST['pr']." sgd");
+                $_SESSION['basketStuff']->removeProduct($product);
+                header("Location: {$_SERVER['HTTP_REFERER']}");
+            }
+        ?>
     </form>
 </div>
