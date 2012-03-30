c1144b5a5df648bfac5e02b72d1a044c6f1e8e46
Demid Merzlyakov <Demid@Demid-notebook.(none)>
2012-03-31 03:10:56 +0600

    mailSender first commit

---
+++ application/controllers/business_logic/mailSender.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once "orderInfo.php";
+
+include_once "userInfo.php";
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of mailSender
+ *
+ * @author Demid
+ */
+class MailSender {
+    //put your code here
+    public static function sendMail(OrderInfo $orderInfo){
+        //самая тяжелая часть работы :'(
+        $userInfo=$orderInfo->userInfo;
+        $recipient=$userInfo->email;
+        
+        $subject="Спасибо за покупку в интернет магазине NoNameShoes.ru!";
+        $message="Здравствуйте, $userInfo->fio!\n
+            Вы приобрели:\n";
+        foreach ($orderInfo->orderedProductsInfo as $product){
+            $message.="$product->name $product->orderedSize размера по цене $product->price руб.\n";
+        }
+        $message.="Большое спасибо за совершение покупки в нашем интернет магазине!";
+        send_email($recipient, $subject, $message);
+    }
+}
+
+?>
